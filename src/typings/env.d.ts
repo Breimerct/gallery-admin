aef0a9343b6ade767019144f9387c207053563c7
Breimerct98 <breimer.correa@zabud.com.co>
2024-08-06 21:23:56 -0500

    chore: Remove unused environment type declaration file

--- src/typings/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PORT: string;
-    JWT_SECRET_KEY: string;
-    JWT_EXPIRES_IN: string;
-    MONGODB_URI: string;
-    SALT_OR_ROUNDS: string;
-  }
-}
