92831a2f7b1a0b327cad73b1ba86ab9169f35b23
Breimerct98 <breimer.correa@zabud.com.co>
2024-08-11 20:29:18 -0500

    chore: add UI avatar URL constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,8 @@
-import { join } from "path";
+import { join } from 'path';
 
 export const rootPath = join(__dirname, '..', 'public/images');
+
+export const UI_AVATAR_URL_BASE =
+  'https://ui-avatars.com/api/?bold=true&uppercase=true';
+
+  export const { JWT_EXPIRES_IN, JWT_SECRET_KEY } = process.env;
