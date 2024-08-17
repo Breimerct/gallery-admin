9eaf214479830dc91e19f182312419412ec1400b
Breimerct98 <breimer.correa@zabud.com.co>
2024-08-17 11:37:41 -0500

    feat: Update rootPath constant to use relative path for images

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,6 @@
-import { join } from 'path';
-
-export const rootPath = join(__dirname, '..', 'public/images');
+export const rootPath = process.cwd() + '/src/images';
 
 export const UI_AVATAR_URL_BASE =
   'https://ui-avatars.com/api/?bold=true&uppercase=true';
 
-  export const { JWT_EXPIRES_IN, JWT_SECRET_KEY } = process.env;
+export const { JWT_EXPIRES_IN, JWT_SECRET_KEY } = process.env;
